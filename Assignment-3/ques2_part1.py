2e3dacdeb992116bdb135e6941338b61146ec39f
Bhavin Agarwal <Bhavin Agarwal>
2017-04-05 23:17:33 -0400

    Assignmnet-3

--- Assignment-3/ques2_part1.py
+++ Assignment-3/ques2_part1.py
@@ -6,12 +6,9 @@ df = df.rename(columns={'Organization Group Code':'Organization_Group_Code','Org
 												#renaming the headers
 df1=df.groupby(['Organization_Group','Department']).Total_Compensation.mean().to_frame()
 											    #grouping data by Organization_Group and Department and calculating Total_Compensation's mean
-output_df=pd.DataFrame(columns=['Organization_Group','Department','Total_Compensation'])
-												#creating a new dataframe
-for index, row in df1.iterrows():				#writing the data to the dataframe
-    output_df=output_df.append(pd.Series([index[0],index[1],row[0]],index=['Organization_Group','Department','Total_Compensation']),ignore_index=True)
-												#appending data to the dataframe
-output_df = output_df.sort_values(['Organization_Group','Total_Compensation'], ascending=[True,False])
+df1=df1.reset_index()
+												#reseting the index
+df1 = df1.sort_values(['Organization_Group','Total_Compensation'], ascending=[True,False])
 												#sorting data for each Organization_Group
-output_df.to_csv('ques2_part1.csv')				#writing data to a csv
-print(output_df.head())
+df1.to_csv('ques2_part1.csv')				    #writing data to a csv
+print(df1.head())
